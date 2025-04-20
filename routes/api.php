1a165222f415ecc7bd362593a45c44aa99f72e1d
santiago calligari <carlossantiagocalligari@gmail.com>
2025-04-20 02:32:35 -0300

    Added full user functionalities, added University

--- routes/api.php
+++ routes/api.php
@@ -7,4 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/auth/token', [AuthController::class, 'store_token']);
 Route::post('/users', [UserController::class, 'store']);
 
-Route::middleware('auth:api')->group(function () {});
+Route::middleware('auth:api')->group(function () {
+    Route::get('/users', [UserController::class, 'show']);
+    Route::post('/users/update', [UserController::class, 'update']);
+});
