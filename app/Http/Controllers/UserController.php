7ab2859966a644783f93cd63551237d05bb7b001
santiago calligari <santiago@calligari.isonote>
2025-04-20 02:32:35 -0300

    Added full user functionalities, added University

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreUserRequest;
 use App\Http\Resources\UserResource;
+use App\Http\Requests\UpdateUserRequest;
 use App\Models\Role;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -14,5 +16,17 @@ class UserController extends Controller
         $user = User::query()->create($request->validated());
         $user->assignRole(Role::USER);
         return new UserResource($user);
+    }
+
+    public function update(UpdateUserRequest $request): UserResource
+    {
+        $user = $request->user;
+        $user->update($request->validated());
+        return new UserResource($user);
+    }
+
+    public function show(): UserResource
+    {
+        return new UserResource(Auth::user());
     }
 }
